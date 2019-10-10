6dd0cd7011a59b104e8a91f22c148a6774ea4e87
mjlbnu <mjlbnu@gmail.com>
2019-10-10 20:42:06 -0300

    Second deploy with frontend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  //baseURL: 'https://protected-plateau-38218.herokuapp.com',
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://thawing-savannah-09389.herokuapp.com',
+  //baseURL: 'http://localhost:3333',
 });
 
 export default api;
